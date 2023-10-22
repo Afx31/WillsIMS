8b6d341afc90f09db47bd1b331ef25d4e296644d
Afx31 <post4wjm@gmail.com>
2023-10-22 20:33:41 +1100

    Encapsulation for InventoryItemBinLocation junction table

--- Controllers/InventoryItemBinLocationController.cs
+++ Controllers/InventoryItemBinLocationController.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
-using System.Data;
 using WillsIMS.Models;
+using WillsIMS.Repositories;
 
 namespace WillsIMS.Controllers
 {
     public class InventoryItemBinLocationController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly InventoryItemRepository _inventoryItemBinLocationRepository;
 
         public InventoryItemBinLocationController(IConfiguration configuration)
         {
             _configuration = configuration;
+            _inventoryItemBinLocationRepository = new InventoryItemRepository(_configuration.GetConnectionString("DatabaseConnection"));
         }
 
         [HttpGet(ApiEndpoints.InventoryItemBinLocation.GetAll)]
@@ -19,46 +20,7 @@ namespace WillsIMS.Controllers
         {
             try
             {
-                string query = @"
-                            SELECT *
-                            FROM InventoryItemBinLocation
-                            ";
-
-                DataTable dt = new DataTable();
-                string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
-                SqlDataReader myReader;
-
-                using (SqlConnection myConnection = new SqlConnection(sqlDataSource))
-                {
-                    myConnection.Open();
-
-                    using (SqlCommand myCommand = new SqlCommand(query, myConnection))
-                    {
-                        myReader = myCommand.ExecuteReader();
-                        dt.Load(myReader);
-                        myReader.Close();
-                        myConnection.Close();
-                    }
-                }
-
-
-                List<InventoryItemBinLocation> inventoryItemBinLocations = new List<InventoryItemBinLocation>();
-
-                foreach (DataRow row in dt.Rows)
-                {
-                    InventoryItemBinLocation inventoryItemBinLocation = new InventoryItemBinLocation
-                    {
-                        InventoryItemBinLocationId = Convert.ToInt32(row["InventoryItemBinLocationId"]),
-                        InventoryItemId = Convert.ToInt32(row["InventoryItemId"]),
-                        BinLocationId = Convert.ToInt32(row["BinLocationId"])
-                    };
-
-                    //inventoryItemBinLocation.InventoryItem = GetInventoryItem(inventoryItemBinLocation.InventoryItemId);
-                    //inventoryItemBinLocation.BinLocation = GetBinLocation(inventoryItemBinLocation.BinLocationId);
-
-                    inventoryItemBinLocations.Add(inventoryItemBinLocation);
-                }
-
+                List<InventoryItem> inventoryItemBinLocations = _inventoryItemBinLocationRepository.GetAllInventoryItemBinLocations();
                 return Ok(inventoryItemBinLocations);
             }
             catch (Exception ex)
---
+++ Repositories/InventoryItemBinLocationRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using WillsIMS.Models;
+
+namespace WillsIMS.Repositories
+{
+    public class InventoryItemBinLocationRepository
+    {
+        private readonly string _connectionString;
+
+        public InventoryItemBinLocationRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<InventoryItemBinLocation> GetAllInventoryItemBinLocations()
+        {
+            try
+            {
+                string query = @"
+                            SELECT *
+                            FROM InventoryItemBinLocation
+                            ";
+
+                DataTable dt = new DataTable();
+                SqlDataReader reader;
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        reader = command.ExecuteReader();
+                        dt.Load(reader);
+                        reader.Close();
+                        connection.Close();
+                    }
+                }
+
+                List<InventoryItemBinLocation> inventoryItemBinLocations = new List<InventoryItemBinLocation>();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    InventoryItemBinLocation inventoryItemBinLocation = new InventoryItemBinLocation
+                    {
+                        InventoryItemBinLocationId = Convert.ToInt32(row["InventoryItemBinLocationId"]),
+                        InventoryItemId = Convert.ToInt32(row["InventoryItemId"]),
+                        BinLocationId = Convert.ToInt32(row["BinLocationId"])
+                    };
+
+                    //inventoryItemBinLocation.InventoryItem = GetInventoryItem(inventoryItemBinLocation.InventoryItemId);
+                    //inventoryItemBinLocation.BinLocation = GetBinLocation(inventoryItemBinLocation.BinLocationId);
+
+                    inventoryItemBinLocations.Add(inventoryItemBinLocation);
+                }
+
+                return inventoryItemBinLocations;
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("Exception has occurred in the Inventory Item Bin Location data operations.");
+            }
+        }
+    }
+}
